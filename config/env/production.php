a2004ea6db5d4aca963627814142fc5e4058e0ef
Dima Markus <dmi3000@gmail.com>
2014-04-05 17:25:07 -0400

    Fix the database password for production
    Signed-off-by: Dima Markus <dmi3000@gmail.com>

--- config/env/production.php
+++ config/env/production.php
@@ -15,7 +15,7 @@ define('DB_NAME', 'wp_dima_live');
 define('DB_USER', 'wordpress');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'wordpress');
+define('DB_PASSWORD', 'LSseYcpOD1');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
